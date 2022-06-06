78deb43f9ec40dde63554676ba72095be9ee3511
SigurdJanson <emailjs@web.de>
2022-06-06 21:43:28 +0200

    Added first draft of RenderFragment demo

---
+++ Shared/FontAwesome.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace RenderFragments.Shared;
+
+public static class FontAwesome
+{
+    public static class Solid
+    {
+        public static readonly RenderFragment User =
+            (__builder) => {
+                __builder.AddMarkupContent(0, "<i class=\"fa-solid fa-user\"></i>");
+            };
+
+        public static readonly RenderFragment Anchor =
+        (__builder) => {
+            __builder.AddMarkupContent(0, "<i class=\"fa-solid fa-anchor\"></i>");
+    
+        };
+    }
+
+}
